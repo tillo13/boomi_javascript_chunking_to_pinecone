4f801a156e83f1f0bb3af8a3863554eb86a721f9
Andy Tillo <andy.tillo@gmail.com>
2023-06-20 19:00:35 -0700

    removed the DDP_script_status and fixed the JSON into embeddings

--- boomi_chunking.js
+++ boomi_chunking.js
@@ -1,111 +1,113 @@
-// Load compatibility script
 load("nashorn:mozilla_compat.js");
-// Load Java classes
 importClass(com.boomi.execution.ExecutionUtil);
 importClass(java.io.ByteArrayInputStream);
 importClass(java.util.Properties);
 importClass(java.nio.charset.StandardCharsets);
 
+function escapeSlackJson(input) {
+    input = input.replace(/["“”]/g, "'");
+    input = input.replace(/[‘’]/g, "'");
+    input = input.replace(/[—–]/g, "-");
+    input = input.replace(/…/g, "...");
+    return input.replace(/\\/g, '\\\\')
+        .replace(/\n/g, '\\n')
+        .replace(/\r/g, '')
+        .replace(/\s+/g, ' ');
+}
+
 try {
-  // Document Loop
-  for (var i = 0; i < dataContext.getDataCount(); i++) {
-    var is = dataContext.getStream(i);
-    var props = dataContext.getProperties(i);
+    for (var i = 0; i < dataContext.getDataCount(); i++) {
+        var is = dataContext.getStream(i);
+        var props = dataContext.getProperties(i);
 
-    // Get Dynamic Document Property DDP_incoming_text
-    var propValue = props.getProperty("document.dynamic.userdefined.DDP_incoming_text");
+        var propValue = props.getProperty("document.dynamic.userdefined.DDP_incoming_text");
 
-    // Check if the property is blank or null
-    if (!propValue || propValue.trim() === "") {
-      propValue = "default"; // Replace "default" with your desired default value
-    }
-
-    // Replace newlines and carriage returns with spaces
-    propValue = propValue.replace(/[\r\n]+/g, ' ');
-
-    // Get Dynamic Document Property DDP_max_characters or use default 1500
-    var maxCharsValue = props.getProperty("document.dynamic.userdefined.DDP_max_characters");
-    var MaxCharsPerChunk = 1500;
-    if (!isNaN(maxCharsValue) && parseInt(maxCharsValue) >= 1) {
-      MaxCharsPerChunk = parseInt(maxCharsValue);}
-
-// Begin Chunking Logic
-var title = "Your Title Here"; // Replace with your desired title or dynamic process property
-var chunks = [];
-
-// Splitting text function
-function splitText() {
-  for (var index = 0; index < propValue.length; index += MaxCharsPerChunk) {
-    chunks.push({
-      model: "text-embedding-ada-002",
-      input: propValue.slice(index, index + MaxCharsPerChunk),
-    });
-  }
-}
-
-if (MaxCharsPerChunk === 1 || !propValue.match(/[\.\!\?]/g)) {
-  splitText();
-} else {
-  try {
-    var sentences = propValue.match(/[^\.!\?]+[\.!\?]+/g);
-
-    if (sentences == null) {
-      splitText();
-    } else {
-
-      var chunkStart = 0;
-      while (chunkStart < sentences.length) {
-        var charCount = 0;
-        var chunkText = "";
-        var chunkSentences = 0;
-
-        for (var j = chunkStart; j < sentences.length && charCount < MaxCharsPerChunk; j++) {
-          var sentence = sentences[j];
-          var sentenceCharCount = sentence.length;
-
-          if (sentenceCharCount > MaxCharsPerChunk) {
-            continue;
-          }
-
-          if (charCount + sentenceCharCount <= MaxCharsPerChunk) {
-            charCount += sentenceCharCount;
-            chunkText += " " + sentence;
-            chunkSentences++;
-          } else {
-            break;
-          }
+        if (!propValue || propValue.trim() === "") {
+            propValue = "default";
         }
 
-        var trimmedText = chunkText.trim();
+        propValue = escapeSlackJson(propValue);
+        propValue = propValue.replace(/[\r\n]+/g, ' ');
 
-        if (trimmedText.length > 0) {
-          chunks.push({
-            model: "text-embedding-ada-002",
-            input: trimmedText,
-          });
+        var maxCharsValue = props.getProperty("document.dynamic.userdefined.DDP_max_characters");
+        var MaxCharsPerChunk = 1500;
+        if (!isNaN(maxCharsValue) && parseInt(maxCharsValue) >= 1) {
+            MaxCharsPerChunk = parseInt(maxCharsValue);
         }
 
-        chunkStart += chunkSentences;
-      }
+        var chunks = [];
+
+        function splitText() {
+            for (var index = 0; index < propValue.length; index += MaxCharsPerChunk) {
+                chunks.push({
+                    model: "text-embedding-ada-002",
+                    input: propValue.slice(index, index + MaxCharsPerChunk),
+                });
+            }
+        }
+
+        if (MaxCharsPerChunk === 1 || !propValue.match(/[.!?]/g)) {
+            splitText();
+        } else {
+            try {
+                var sentences = propValue.match(/[^.!?]+[.!?]+/g);
+
+                if (sentences == null) {
+                    splitText();
+                } else {
+
+                    var chunkStart = 0;
+                    while (chunkStart < sentences.length) {
+                        var charCount = 0;
+                        var chunkText = "";
+                        var chunkSentences = 0;
+
+                        for (var j = chunkStart; j < sentences.length && charCount < MaxCharsPerChunk; j++) {
+                            var sentence = sentences[j];
+                            var sentenceCharCount = sentence.length;
+
+                            if (sentenceCharCount > MaxCharsPerChunk) {
+                                continue;
+                            }
+
+                            if (charCount + sentenceCharCount <= MaxCharsPerChunk) {
+                                charCount += sentenceCharCount;
+                                chunkText += " " + sentence;
+                                chunkSentences++;
+                            } else {
+                                break;
+                            }
+                        }
+
+                        var trimmedText = chunkText.trim();
+
+                        if (trimmedText.length > 0) {
+                            chunks.push({
+                                model: "text-embedding-ada-002",
+                                input: trimmedText,
+                            });
+                        }
+
+                        chunkStart += chunkSentences;
+                    }
+                }
+            } catch (e) {
+                splitText();
+            }
+        }
+
+        for (var chunkIndex = 0; chunkIndex < chunks.length; chunkIndex++) {
+            var chunk = chunks[chunkIndex];
+            var jsonPayload = JSON.stringify(chunk);
+            var byteArray = new java.lang.String(jsonPayload).getBytes(StandardCharsets.UTF_8);
+            var chunkStream = new ByteArrayInputStream(byteArray);
+
+            var outputProps = new java.util.Properties();
+            outputProps.put("document.dynamic.userdefined.DDP_outgoing_text", jsonPayload);
+
+            dataContext.storeStream(chunkStream, outputProps);
+        }
     }
-  } catch (e) {
-    splitText();
-  }
-}
-
-// Create separate documents for each chunk
-for (var chunkIndex = 0; chunkIndex < chunks.length; chunkIndex++) {
-  var chunk = chunks[chunkIndex];
-  var jsonPayload = JSON.stringify(chunk);
-  var byteArray = new java.lang.String(jsonPayload).getBytes(StandardCharsets.UTF_8);
-  var chunkStream = new ByteArrayInputStream(byteArray);
-
-  dataContext.storeStream(chunkStream, props);
-}
-
-// End Chunking Logic
-}
 } catch (error) {
-// Catch any unexpected errors
-logger.severe("An error occurred during script execution: " + error);
+    logger.severe("An error occurred during script execution: " + error);
 }
